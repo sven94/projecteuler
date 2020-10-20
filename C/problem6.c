6d4e17c90857d8480a62829539dc0e334bd1591d
Sven Pors <pors@ese.eur.nl>
2020-10-20 22:00:05 +0200

    Added problem 6 and 7

---
+++ C/problem6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h> 
+
+// Problem 6
+int main() {
+    int n = 100;
+    int squareOfSums = 0;
+    int sumOfSquares = 0;
+
+    for (int i = 1; i <= n; i++) {
+        squareOfSums += i;
+        sumOfSquares += pow(i, 2);
+    }
+    squareOfSums = pow(squareOfSums, 2);
+    printf("Difference between square of sums and sum of squares: %d\n", squareOfSums - sumOfSquares);
+}
